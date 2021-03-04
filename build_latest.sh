9ba01bac8578c4a8ef7ecfa16d25288b1581fb25
visualfc <visualfc@gmail.com>
2021-03-04 12:16:56 +0800

    gop v0.7.18

--- build_latest.sh
+++ build_latest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go mod edit -require=github.com/goplus/gop@latest
-go list --tags js
+go mod download github.com/goplus/gop
 GOARCH=wasm GOOS=js go build -o igop.wasm
 gopherjs build -v -m -o igop.js
